3989eb89c7151dd2efdd965065d02ba37b441abd
_____ <22010176@st.phenikaa-uni.edu.vn>
2023-11-06 12:11:20 +0700

    ed

--- old/_v1/pages/Login/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles.scss"
-
-export default function ({ name, onClick }) {
-  function _onClick(e) { if (typeof onClick == 'function') onClick(e) }
-  return (
-    <button className="button-1" role="button" onClick={_onClick}>{name}</button>
-  )
-
-}
--- old/_v1/pages/components/Navbar/index.jsx
+++
@@ -1,9 +0,0 @@
-import './styles.scss'
-
-export default function () {
-  return (
-    <div>
-      <h1>Hello Wolrd</h1>
-    </div>
-  )
-}
