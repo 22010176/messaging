6946fbb0f7d791d89c849a5b8bc8642da8cc2afb
_____ <22010176@st.phenikaa-uni.edu.vn>
2023-11-07 14:31:45 +0700

    update-client

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -8,20 +8,22 @@ import './App.css';
 
 function App() {
   const socket = socketClient()
-
+  socket.on('connection', () => {
+    console.log("Hello")
+  })
   useEffect(function () {
     // fetch('/api').then(res => res.text()).then(console.log)
   }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
