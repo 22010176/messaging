305364b8f5c9c31fe0b5017bff67b9d81002bfdb
_____ <22010176@st.phenikaa-uni.edu.vn>
2023-11-08 23:36:15 +0700

    ed

--- database/index.js
+++ database/index.js
@@ -1,5 +1,15 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({ user: 'root', password: 'abcd1234', });
+const connection = mysql.createConnection({ user: 'root', password: 'abcd1234' });
+
+
+connection.query(`
+CREATE DATABASE IF NOT EXISTS testDB;
+USE testDB;
+`)
+// connection.connect()
+// connection.query("")
+
+// connection.end();
 
 module.exports = connection
--- server/index.js
+++ server/index.js
@@ -15,8 +15,10 @@ app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 app.get("/api", (req, res) => res.json({ message: "Hello from server!" }));
 
-connection.connect(error => { if (error) throw error; });
+
+
 server.listen(PORT, () => console.log("Database connection is Ready and Server is Listening on Port ", PORT))
+
 io.on('connection', socket => {
   console.log("New");
   socket.emit('connection', null);
