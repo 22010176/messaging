1af3079a434338537f48400532d7faaaf271c4b0
_____ <22010176@st.phenikaa-uni.edu.vn>
2023-11-10 14:07:32 +0700

    ed

--- client/src/pages/Chats/index.jsx
+++ client/src/pages/Chats/index.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss'
 import { v4 } from 'uuid';
 
-
-
 export default function App() {
   const [name, setName] = useState("")
+  const [message, setMessage] = useState("Hello")
 
   useEffect(function () {
     const _name = localStorage.getItem('user')
@@ -21,7 +23,22 @@ export default function App() {
   return (
     <div className={[styles['chat-app']].join(' ')}>
       <div className={[styles['message-box'], styles.container].join(' ')}>
-
+        <div className={styles.message}>
+          <div className={styles['user-message']}>
+            {name}
+          </div>
+          <div className={styles['messaage']}>
+            {message}
+          </div>
+        </div>
+        <div className={styles.message}>
+          <div className={styles['user-message']}>
+            {name}
+          </div>
+          <div className={styles['messaage']}>
+            {message}
+          </div>
+        </div>
       </div>
       <div className={[styles['message-form'], styles.container].join(' ')}>
         <div className={styles['form-wrapper']}></div>
