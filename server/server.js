9384745330582e024913c96a994b5cd3cd35c71f
_____ <22010176@st.phenikaa-uni.edu.vn>
2023-11-12 11:38:02 +0700

    ed

--- server/server.js
+++ server/server.js
@@ -14,13 +14,6 @@ app.use(require('cors')({ origin: ['http://127.0.0.1:3001'] }))
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 app.use('/api', require('./Route'))
-// app.get("/api", (req, res) => res.json({ message: "Hello from server!" }));
 
 module.exports = { io, server, PORT }
 
-// server.listen(PORT, () => console.log("Database connection is Ready and Server is Listening on Port ", PORT))
-
-// io.on('connection', socket => {
-//   console.log("New");
-//   socket.emit('connection', null);
-// })
